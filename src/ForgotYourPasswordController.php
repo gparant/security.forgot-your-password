9a2269076e1991085c87a6d8d92bea28b383b71f
Unknown <gregoire57710@hotmail.fr>
2017-09-22 09:46:48 +0200

    fix route
    add parameter

--- src/ForgotYourPasswordController.php
+++ src/ForgotYourPasswordController.php
@@ -235,7 +235,7 @@ class ForgotYourPasswordController
     }
 
     /**
-     * @URL("{$this->baseUrl}/reset")
+     * @URL("{$this->baseUrl}/reset/{token}")
      * @Get()
      *
      * @param string $token
